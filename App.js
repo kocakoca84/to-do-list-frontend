187b7c8a1b548134b62a21d1835d4d2367deb202
kocakoca84 <51060820+kocakoca84@users.noreply.github.com>
2020-02-05 01:09:26 +0100

    BUG1: Updated code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -10,53 +10,74 @@ class App extends Component {
     }
   }
 
-  componentWillMount(){
+  componentDidMount() {
     fetch('http://localhost:59585/api/lists/get')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ toDoTasks: data })
-    })
-  }
-
-  updateInput(key, value){
-    this.setState({
-      [key]:value
-    })
-  }
-
-  addItem(){
-    fetch('http://localhost:59585/api/lists/create',
-      {
-        method: 'POST',
-        body: JSON.stringify({"description": this.state.newTask})
-     })
       .then(res => res.json())
       .then((data) => {
-        const newTask={
-          id:data.id,
-          description: this.state.newTask.slice()
-        };
-        const toDoTasks=[...this.state.toDoTasks];
-        toDoTasks.push(newTask);
-        this.setState({
-          toDoTasks,
-          newTask:""
-        })
+        this.setState({ toDoTasks: data })
       })
   }
-  
-  updateTask(id){
-    const toDoTasks = [...this.state.toDoTasks];
-    
-    const updatedTasks = toDoTasks.filter(task => task.id !== id);
 
-    this.setState({toDoTasks: updatedTasks});
+  updateInput(key, value) {
+    this.setState({
+      [key]: value
+    })
+  }
+
+  addItem() {
+    const request = async () => {
+      const response = await fetch('http://localhost:59585/api/lists/create',
+        {
+          mode: "cors",
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Origin': 'http://localhost:3000'
+          },
+          body: JSON.stringify({ description: this.state.newTask })
+        });
+
+      const json = await response.json();
+
+      const newTask = {
+        id: json,
+        description: this.state.newTask.slice(),
+        completed: false
+      };
+      const toDoTasks = [...this.state.toDoTasks];
+      toDoTasks.push(newTask);
+      this.setState({
+        toDoTasks,
+        newTask: ""
+      });
+    };
+
+    request();
+  }
+
+  updateTask(id) {
+    const toDoTasks = [...this.state.toDoTasks];
+    var index = toDoTasks.findIndex(t => t.id === id);
+    if (toDoTasks[index].completed) {
+      toDoTasks[index].completed = false;
+    }
+    else {
+      toDoTasks[index].completed = true;
+    }
+    this.setState({ toDoTasks: toDoTasks });
 
     fetch('http://localhost:59585/api/lists/update',
       {
+        mode: "cors",
         method: 'POST',
-        body: JSON.stringify({"id": id})
-     })
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Origin': 'http://localhost:3000'
+        },
+        body: JSON.stringify({ "id": id })
+      })
   }
 
   render() {
@@ -69,7 +90,7 @@ class App extends Component {
               return (
                 <li key={task.id}>
                   {task.description}
-                  <button 
+                  <button
                     onClick={() => this.updateTask(task.id)}
                   >Done!</button>
                 </li>
@@ -79,25 +100,25 @@ class App extends Component {
         </div>
         <div>
           Add an item...
-          <br/>
-          <input 
+          <br />
+          <input
             type="text"
             placeholder="Enter description..."
             value={this.state.newTask}
-            onChange={e=> this.updateInput("newTask", e.target.value)}
+            onChange={e => this.updateInput("newTask", e.target.value)}
           />
           <button onClick={() => this.addItem()}>Add Task
           </button>
-          </div>
-          <br/>
-          <div>
-            Completed tasks
+        </div>
+        <br />
+        <div>
+          Completed tasks
           <ul>
             {this.state.toDoTasks.filter((task) => task.completed === true).map(task => {
               return (
                 <li key={task.id}>
                   {task.description}
-                  <button 
+                  <button
                     onClick={() => this.updateTask(task.id)}
                   >Undo</button>
                 </li>
